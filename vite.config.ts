aefa40c0f1bc2bb8446d2206b3ff73e47c454f93
Grigorii Veinin <kage.grifon@yandex.ru>
2026-03-30 14:08:39 +0700

    config: change deploy url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/quiz/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,4 +12,3 @@ export default defineConfig({
     },
   },
 });
-
